b0734208112233eddb3bb3a9b45b4a8fa8cc9d4e
Amirhosseinnk81 <96089096+Amirhosseinnk81@users.noreply.github.com>
2023-08-26 16:52:29 -0700

    'v1.6'

--- MY_BLOG/settings.py
+++ MY_BLOG/settings.py
@@ -112,9 +112,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR , 'Static')
+STATIC_URL = 'Static/'
+STATICFILES_DIRS = [ BASE_DIR / "Static" ]
+# STATIC_ROOT = os.path.join(BASE_DIR , 'Static')
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 MEDIA_URL = 'media/'
 
